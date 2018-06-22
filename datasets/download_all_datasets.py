9b35504e4f9fcba7eb6c899933e599fa02a6dbab
Alexander Pacha <alexander.pacha@gmail.com>
2018-06-22 13:05:58 +0200

    Moving all extracted datasets into data folder and ignoring that folder. Fixing calls to downloaders (path is now in download-method instead of constructor).

--- datasets/download_all_datasets.py
+++ datasets/download_all_datasets.py
@@ -12,23 +12,23 @@ if __name__ == "__main__":
     parser.add_argument(
         "--dataset_directory",
         type=str,
-        default=".",
+        default="../data",
         help="The directory, where the extracted dataset will be copied to")
 
     flags, unparsed = parser.parse_known_args()
 
     muscima_pp_directory = os.path.join(flags.dataset_directory, "muscima_pp")
-    downloader = MuscimaPlusPlusDatasetDownloader(muscima_pp_directory)
-    downloader.download_and_extract_dataset()
+    downloader = MuscimaPlusPlusDatasetDownloader()
+    downloader.download_and_extract_dataset(muscima_pp_directory)
 
     cvc_muscima_directory = os.path.join(flags.dataset_directory, "cvc_muscima")
-    downloader = CvcMuscimaDatasetDownloader(cvc_muscima_directory, CvcMuscimaDataset.StaffRemoval)
-    downloader.download_and_extract_dataset()
+    downloader = CvcMuscimaDatasetDownloader(CvcMuscimaDataset.StaffRemoval)
+    downloader.download_and_extract_dataset(cvc_muscima_directory)
 
     mensural_directory = os.path.join(flags.dataset_directory, "mensural")
-    downloader = MensuralDatasetDownloader(mensural_directory)
-    downloader.download_and_extract_dataset()
+    downloader = MensuralDatasetDownloader()
+    downloader.download_and_extract_dataset(mensural_directory)
 
     deepscores_directory = os.path.join(flags.dataset_directory, "deepscores")
-    downloader = DeepScoresDatasetDownloader(deepscores_directory)
-    downloader.download_and_extract_dataset()
+    downloader = DeepScoresDatasetDownloader()
+    downloader.download_and_extract_dataset(deepscores_directory)
