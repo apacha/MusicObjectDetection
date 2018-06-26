c6ed17aa55d0d58d5fb3dac598569d1ba39939c1
Alexander Pacha <alexander.pacha@gmail.com>
2018-06-26 13:18:36 +0200

    Adding normalization for mensural dataset

---
+++ preprocessing/MensuralConverter.py
@@ -0,0 +1,45 @@
+import os
+from glob import glob
+
+from PIL import Image
+from tqdm import tqdm
+import pandas as pd
+
+
+class MensuralConverter(object):
+
+    def copy_and_normalize_images(self, mensural_directory: str, output_directory: str) -> None:
+        os.makedirs(os.path.join(output_directory, "images"), exist_ok=True)
+
+        all_images = glob(mensural_directory + "/*.JPG")
+
+        for image_path in tqdm(all_images, desc="Copying and converting images"):
+            image = Image.open(image_path)
+            filename = os.path.splitext(os.path.basename(image_path))[0] + ".png"
+            output_path = os.path.join(output_directory, "images", filename)
+            image.save(output_path)
+
+    def normalize_annotations(self, mensural_directory: str, output_directory: str) -> None:
+        destination_annotation_file = os.path.join(output_directory, "annotations.csv")
+        all_annotations = glob(mensural_directory + "/*.txt")
+
+        data = []
+        for annotation_path in tqdm(all_annotations, desc="Normalizing annotations"):
+            with open(annotation_path, 'r') as gt_file:
+                lines = gt_file.read().splitlines()
+
+            filename = "images/{0}.png".format(os.path.basename(annotation_path[-8]))
+            for line in lines:
+                upper_left, lower_right, class_name, gt_position = line.split(';')
+                xmin, ymin = upper_left.split(',')
+                xmax, ymax = lower_right.split(',')
+
+                top = float(ymin)
+                left = float(xmin)
+                bottom = float(ymax)
+                right = float(xmax)
+                data.append((filename, top, left, bottom, right, class_name))
+
+        all_annotations = pd.DataFrame(data=data,
+                                       columns=["path_to_image", "top", "left", "bottom", "right", "class_name"])
+        all_annotations.to_csv(destination_annotation_file, index=False, float_format="%.0f")
--- preprocessing/normalize_all_datasets.py
+++ preprocessing/normalize_all_datasets.py
@@ -2,6 +2,7 @@ import argparse
 import os
 
 from preprocessing.DeepScoreXmlToCsvConverter import DeepScoreXmlToCsvConverter
+from preprocessing.MensuralConverter import MensuralConverter
 from preprocessing.MuscimaPpXmlToCsvConverter import MuscimaPpXmlToCsvConverter
 
 if __name__ == "__main__":
@@ -17,11 +18,18 @@ if __name__ == "__main__":
     deep_score_converter = DeepScoreXmlToCsvConverter()
     deep_score_directory = os.path.join(flags.dataset_directory, "deepscores", "deep_scores_dense")
     normalized_deep_score_directory = os.path.join(flags.dataset_directory, "normalized", "deepscores")
-    deep_score_converter.convert_and_normalize_deep_scores_dataset(deep_score_directory,
-                                                                   normalized_deep_score_directory)
+    deep_score_converter.copy_and_normalize_images(deep_score_directory, normalized_deep_score_directory)
+    deep_score_converter.normalize_annotations(deep_score_directory, normalized_deep_score_directory)
 
     muscima_pp_converter = MuscimaPpXmlToCsvConverter()
+    normalized_muscima_pp_directory = os.path.join(flags.dataset_directory, "normalized", "muscima")
     muscima_pp_converter.copy_and_normalize_images(os.path.join(flags.dataset_directory, "cvc_muscima"),
-                                                   os.path.join(flags.dataset_directory, "normalized", "muscima"))
+                                                   normalized_muscima_pp_directory)
     muscima_pp_converter.normalize_annotations(os.path.join(flags.dataset_directory, "muscima_pp"),
-                                               os.path.join(flags.dataset_directory, "normalized", "muscima"))
+                                               normalized_muscima_pp_directory)
+
+    mensural_converter = MensuralConverter()
+    normalized_mensural_directory = os.path.join(flags.dataset_directory, "normalized", "mensural")
+    mensural_directory = os.path.join(flags.dataset_directory, "mensural")
+    mensural_converter.copy_and_normalize_images(mensural_directory, normalized_mensural_directory)
+    mensural_converter.normalize_annotations(mensural_directory, normalized_mensural_directory)
