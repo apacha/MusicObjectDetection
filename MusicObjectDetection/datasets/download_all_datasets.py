6c88ad946e8214aa008266110f8757dfb13bdd75
Alexander Pacha <alexander.pacha@gmail.com>
2018-07-26 18:43:09 +0200

    Adding test to verify, that every use will get exactly the same dataset-split as we have.

--- MusicObjectDetection/datasets/download_all_datasets.py
+++ MusicObjectDetection/datasets/download_all_datasets.py
@@ -7,6 +7,25 @@ from omrdatasettools.downloaders.MuscimaPlusPlusDatasetDownloader import Muscima
 from MusicObjectDetection.datasets.DeepScoresDatasetDownloader import DeepScoresDatasetDownloader
 from MusicObjectDetection.datasets.MensuralDatasetDownloader import MensuralDatasetDownloader
 
+
+def download_all_datasets(dataset_directory):
+    muscima_pp_directory = os.path.join(dataset_directory, "muscima_pp")
+    downloader = MuscimaPlusPlusDatasetDownloader()
+    downloader.download_and_extract_dataset(muscima_pp_directory)
+
+    cvc_muscima_directory = os.path.join(dataset_directory, "cvc_muscima")
+    downloader = CvcMuscimaDatasetDownloader(CvcMuscimaDataset.StaffRemoval)
+    downloader.download_and_extract_dataset(cvc_muscima_directory)
+
+    mensural_directory = os.path.join(dataset_directory, "mensural")
+    downloader = MensuralDatasetDownloader()
+    downloader.download_and_extract_dataset(mensural_directory)
+
+    deepscores_directory = os.path.join(dataset_directory, "deepscores")
+    downloader = DeepScoresDatasetDownloader()
+    downloader.download_and_extract_dataset(deepscores_directory)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -17,18 +36,5 @@ if __name__ == "__main__":
 
     flags, unparsed = parser.parse_known_args()
 
-    muscima_pp_directory = os.path.join(flags.dataset_directory, "muscima_pp")
-    downloader = MuscimaPlusPlusDatasetDownloader()
-    downloader.download_and_extract_dataset(muscima_pp_directory)
-
-    cvc_muscima_directory = os.path.join(flags.dataset_directory, "cvc_muscima")
-    downloader = CvcMuscimaDatasetDownloader(CvcMuscimaDataset.StaffRemoval)
-    downloader.download_and_extract_dataset(cvc_muscima_directory)
-
-    mensural_directory = os.path.join(flags.dataset_directory, "mensural")
-    downloader = MensuralDatasetDownloader()
-    downloader.download_and_extract_dataset(mensural_directory)
-
-    deepscores_directory = os.path.join(flags.dataset_directory, "deepscores")
-    downloader = DeepScoresDatasetDownloader()
-    downloader.download_and_extract_dataset(deepscores_directory)
+    dataset_directory = flags.dataset_directory
+    download_all_datasets(dataset_directory)
--- MusicObjectDetection/preprocessing/normalize_all_datasets.py
+++ MusicObjectDetection/preprocessing/normalize_all_datasets.py
@@ -6,6 +6,34 @@ from MusicObjectDetection.preprocessing.MensuralConverter import MensuralConvert
 from MusicObjectDetection.preprocessing.MuscimaPpXmlToCsvConverter import MuscimaPpXmlToCsvConverter
 from MusicObjectDetection.preprocessing.dataset_splitter import DatasetSplitter
 
+
+def normalize_all_datasets(dataset_directory: str) -> None:
+    deep_score_converter = DeepScoreXmlToCsvConverter()
+    deep_score_directory = os.path.join(dataset_directory, "deepscores", "deep_scores_v2_100p")
+    normalized_deep_score_directory = os.path.join(dataset_directory, "normalized", "deepscores")
+    deep_score_converter.copy_and_normalize_images(deep_score_directory, normalized_deep_score_directory)
+    deep_score_converter.normalize_annotations(deep_score_directory, normalized_deep_score_directory)
+
+    muscima_pp_converter = MuscimaPpXmlToCsvConverter()
+    normalized_muscima_pp_directory = os.path.join(dataset_directory, "normalized", "muscima")
+    muscima_pp_converter.copy_and_normalize_images(os.path.join(dataset_directory, "cvc_muscima"),
+                                                   normalized_muscima_pp_directory)
+    muscima_pp_converter.normalize_annotations(os.path.join(dataset_directory, "muscima_pp"),
+                                               normalized_muscima_pp_directory)
+    muscima_pp_converter.remove_unused_images(normalized_muscima_pp_directory)
+
+    mensural_converter = MensuralConverter()
+    normalized_mensural_directory = os.path.join(dataset_directory, "normalized", "mensural")
+    mensural_directory = os.path.join(dataset_directory, "mensural")
+    mensural_converter.copy_and_normalize_images(mensural_directory, normalized_mensural_directory)
+    mensural_converter.normalize_annotations(mensural_directory, normalized_mensural_directory)
+
+    dataset_splitter = DatasetSplitter()
+    dataset_splitter.split_annotations_into_training_validation_and_test_set(normalized_deep_score_directory)
+    dataset_splitter.split_annotations_into_training_validation_and_test_set(normalized_muscima_pp_directory)
+    dataset_splitter.split_annotations_into_training_validation_and_test_set(normalized_mensural_directory)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -16,27 +44,4 @@ if __name__ == "__main__":
 
     flags, unparsed = parser.parse_known_args()
 
-    deep_score_converter = DeepScoreXmlToCsvConverter()
-    deep_score_directory = os.path.join(flags.dataset_directory, "deepscores", "deep_scores_v2_100p")
-    normalized_deep_score_directory = os.path.join(flags.dataset_directory, "normalized", "deepscores")
-    deep_score_converter.copy_and_normalize_images(deep_score_directory, normalized_deep_score_directory)
-    deep_score_converter.normalize_annotations(deep_score_directory, normalized_deep_score_directory)
-
-    muscima_pp_converter = MuscimaPpXmlToCsvConverter()
-    normalized_muscima_pp_directory = os.path.join(flags.dataset_directory, "normalized", "muscima")
-    muscima_pp_converter.copy_and_normalize_images(os.path.join(flags.dataset_directory, "cvc_muscima"),
-                                                   normalized_muscima_pp_directory)
-    muscima_pp_converter.normalize_annotations(os.path.join(flags.dataset_directory, "muscima_pp"),
-                                               normalized_muscima_pp_directory)
-    muscima_pp_converter.remove_unused_images(normalized_muscima_pp_directory)
-
-    mensural_converter = MensuralConverter()
-    normalized_mensural_directory = os.path.join(flags.dataset_directory, "normalized", "mensural")
-    mensural_directory = os.path.join(flags.dataset_directory, "mensural")
-    mensural_converter.copy_and_normalize_images(mensural_directory, normalized_mensural_directory)
-    mensural_converter.normalize_annotations(mensural_directory, normalized_mensural_directory)
-
-    dataset_splitter = DatasetSplitter()
-    dataset_splitter.split_annotations_into_training_validation_and_test_set(normalized_deep_score_directory)
-    dataset_splitter.split_annotations_into_training_validation_and_test_set(normalized_muscima_pp_directory)
-    dataset_splitter.split_annotations_into_training_validation_and_test_set(normalized_mensural_directory)
+    normalize_all_datasets(flags.dataset_directory)
